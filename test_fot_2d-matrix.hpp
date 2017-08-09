49e9670c96e04556e267eb3ee9a7068822cd95a7
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-08-09 16:44:38 +0900

    add 2d-matrix package

---
+++ test_fot_2d-matrix.hpp
@@ -0,0 +1,16 @@
+#ifndef UTIL-TEST-TOW-DIMENSION-MATRIX
+#define UTIL-TEST-TOW-DIMENSION-MATRIX
+
+namespace Utilpack{
+
+bool test_tow_dimension_matrix()
+{
+    bool result;
+    
+    return result;
+}
+
+} /* Utilpack */
+
+
+#endif /*UTIL-TEST-TOW-DIMENSION-MATRIX*/
