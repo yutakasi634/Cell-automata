5591957c9b5fed723e5be00096dec4a84de4d85b
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-24 11:27:02 +0900

    remove ising_traits from template parameter and add initialize().

--- Ising_model/spin.hpp
+++ Ising_model/spin.hpp
@@ -6,26 +6,29 @@
 #include <stdexcept>
 #include <cmath>
 #include "spin_base.hpp"
-#include "Ising_traits.hpp"
 
-template<typename ising_traits_T>
+template<typename numerical_T, typename random_engine_T>
 class Neuman_flip_spin : public Spin_base<int, 4>
 {
-    using numerical_type	= typename ising_traits_T::numerical_type;
-    using random_engine_type	= typename ising_traits_T::random_number_generator_type;
+    using numerical_type	= numerical_T;
+    using random_engine_type	= random_engine_T;
     
   public:
     Neuman_flip_spin(state_type s, numerical_type& temp, numerical_type& perm,
-		     numerical_type& spin_inter, random_engine_type& ram):
+		     numerical_type& spin_inter, random_engine_type& ran):
 	Spin_base<int, 4>(s), tempreture(temp), magnetic_flux_density(perm),
-	spin_interaction(spin_inter), random_engine(ram)
+	spin_interaction(spin_inter), random_engine(ran)
     {
 	if(s != 1 && s != -1)
 	    throw std::invalid_argument("Neuman flip spin state have to be 1 or -1.");
 	
 	typename
-	    std::uniform_real_distribution<numerical_type>::param_type param(0.0, 1.0);
-	dist01.param(param);
+	    std::uniform_real_distribution<numerical_type>::param_type param_r(0.0, 1.0);
+	dist_r01.param(param_r);
+
+	typename
+	    std::uniform_int_distribution<>::param_type param_i(0, 1);
+	dist_i01.param(param_i);
     }
 
     virtual void set_partners(const partners_list_type& partners_list) override
@@ -42,7 +45,7 @@ class Neuman_flip_spin : public Spin_base<int, 4>
 	else
 	{
 	    numerical_type flip_prob = std::exp(-tempreture * energy_diff);
-	    if(dist01(random_engine) <= flip_prob)
+	    if(dist_r01(random_engine) <= flip_prob)
 		flip();
 	}
 	return;
@@ -67,11 +70,17 @@ class Neuman_flip_spin : public Spin_base<int, 4>
     {
 	return -current_energy();
     }
+
+    void initialize()
+    {
+	state = 2 * dist_i01(random_engine) - 1;
+    }
     
   private:
     const numerical_type& tempreture, magnetic_flux_density, spin_interaction;
     random_engine_type& random_engine;
-    std::uniform_real_distribution<numerical_type> dist01;
+    std::uniform_real_distribution<numerical_type> dist_r01;
+    std::uniform_int_distribution<> dist_i01;
 };
 
 #endif /* CELL_AUTOMATA_ISING_SPIN */
