15bd9385d0b123ac86fa975c138358ca7a10d0e3
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-28 22:51:32 +0900

    edit to use spin params.

--- Ising_model/system_initializer_func.hpp
+++ Ising_model/system_initializer_func.hpp
@@ -2,59 +2,47 @@
 #define CELL_AUTOMATA_ISING_CONFIGURATOR
 
 #include "../utilpack/array_matrix.hpp"
+#include "spin_params.hpp"
 #include "spin.hpp"
 
+template<typename spin_T, std::size_t row_Num, std::size_t column_Num>
+void system_initialize(
+    Utilpack::array_matrix<spin_T*, row_Num, column_Num>& system,
+    spin_params<spin_T>& params
+    typename spin_T::random_engine_type& random_engine);
+{
+    set_spin<spin_T, row_Num, column_Num>(system, params, random_engine);
+    set_spin_partner<spin_T, row_Num, column_Num>(system);
+    return;
+}
+
+template<typename spin_T, std::size_t row_Num, std::size_t column_Num>
+void set_spin(Utilpack::array_matrix<spin_T*, row_Num, column_Num>& system,
+	      spin_params<spin_T>& params,
+	      typename spin_T::random_engine_type& random_engine);
+{
+    for(spin_T* spin_pointer : system)
+	spin_pointer = new spin_T(params, random_engine);
+    return;
+}
+
+
 template<typename spin_T, std::size_t row_Num, std::size_t column_Num, typename simulator_T>
-void system_initialize(
-    Utilpack::array_matrix_T<spin_T, row_Num, column_Num, simulator_T>&);
+void set_spin_partner(Utilpack::array_matrix<spin_T*, row_Num, column_Num>& system)
+{
+    for(std::size_t row = 0; row < row_Num; ++row)
+	set_row_spin_partner<row_Num, column_Num, simulator_T>(system, row);
+    return;
+}
 
+//set_row_spin_partner have to be overloded for each spin type.
+
+//overload for Neuman_flip_spin
 template<std::size_t row_Num, std::size_t column_Num, typename simulator_T>
-void system_initialize(
-    Utilpack::array_matrix<Neuman_flip_spin*, row_Num, column_Num>& system)
-{
-    using numerical_type = simulator_T::numerical_T;
-    set_spin<Neuman_flip_spin*, row_Num, column_Num, simulator_T>
-	(system, tempreture, magnetic_flux_density, spin_interaction);
-    set_spin_partner<row_Num, column_Num>(system);
-    return;
-}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~set_spin()~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-template<typename spin_T, std::size_t row_Num, std::size_t, typename simulator_T>
-void set_spin(Utilpack::array_matrix<spin_T, row_Num, column_Num>&,
-	      simulator_T::numerical_T , simulator_T::numerical_T , simulator_T::numerical_T );
-
-
-template<std::size_t row_Num, std::size_t column_Num, typename simulator_T>
-void set_spin(Utilpack::array_matrix<Neuman_flip_spin, row_Num, column_Num> system,
-	      simulator_T::numerical_T tempreture,
-	      simulator_T::numerical_T magnetic_flux_density,
-	      simulator_T::numerical_T spin_interaction)
-{
-    for(spin_T spin_pointer : system)
-	spin_pointer = Neuman_flip_spin(
-	    tempreture, magnetic_flux_density, spin_interaction);
-    return;
-}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~set_spin()~~~~~~~~~~~~~~~~~~~~~~~~~~~~END
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~set_spin_partner()~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-template<typename spin_T, std::size_t row_Num, std::size_t column_Num>
-void set_spin_partner(Utilpack::array_matrix<spin_T, row_Num, column_Num>& system)
-{
-    set_row_spin_partner<spin_T, >(system, 0, row_Num, 1);
-    for(std::size_t row; row < row_Num - 1; ++row)
-	set_row_spin_partner(system, row, row - 1, row + 1);
-    set_row_spin_partner(system, row_Num, row_Num - 1, 0);
-    return;
-}
-
-template<typename spin_T, std::size_t row_Num, std::size_t column_Num>
-void set_row_spin_partner(Utilpack::array_matrix<spin_T, row_Num, column_Num>& system,
-			  std::size_t row)
+void set_row_spin_partner(
+    Utilpack::
+    array_matrix<Neuman_flip_spin<simulator_T>*, row_Num, column_Num>& system,
+    std::size_t row)
 {
     std::size_t previous_row, next_row;
     if(row == 0)
@@ -64,7 +52,7 @@ void set_row_spin_partner(Utilpack::array_matrix<spin_T, row_Num, column_Num>& s
     else
 	previous_row = row - 1; next_row = row + 1;
 
-    systemat(row, 0)->set_partners(
+    system.at(row, 0)->set_partners(
 	{&system.at(previous_row, 0), &system.at(row, 1), &system.at(next_row, 0),
 		&system.at(row, column_Num)});
     for(std::size_t column = 1; column < column_Num - 1; ++column)
