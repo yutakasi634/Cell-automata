1e0274141ad998bfc5138769232e35a8ce54c519
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-28 22:47:04 +0900

    add spin_params to abstraction of spin parameter.

---
+++ Ising_model/spin_params.hpp
@@ -0,0 +1,22 @@
+#ifndef CELL_AUTOMATA_ISING_SPIN_PARAMS
+#define CELL_AUTOMATA_ISING_SPIN_PARAMS
+
+template<typename spin_T, typename simulator_T>
+struct spin_params;
+
+
+template<typename simulator_T>
+class Neuman_flip_spin;
+
+template<typename simulator_T>
+struct spin_params<Neuman_flip_spin<simulator_T>, simulator_T>
+{
+    using numerical_type = typename simulator_T::numerical_type;
+
+    spin_params(numerical_type t, numerical_type m, numerical_type s):
+	tempreture(t), magnetic_flux_density(m), spin_interaction(s){};
+    
+    numerical_type tempreture, magnetic_flux_density, spin_interaction;
+};
+
+#endif /* CELL_AUTOMATA_ISING_SPIN_PARAMS */
