9a291a9f5266e3124732425849e81a7c4cd7d7dc
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-28 22:48:45 +0900

    edit to use spin params.

--- Ising_model/test_for_spin.cpp
+++ Ising_model/test_for_spin.cpp
@@ -2,26 +2,29 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
-#include "Ising_simulator_traits.hpp"
-#include "spin.hpp"
 #include <iostream>
 #include <array>
 #include <cmath>
+#include "Ising_simulator_traits.hpp"
+#include "spin.hpp"
+#include "spin_params.hpp"
 
-using numerical_type		= Default_simulator_traits::numerical_type;
-using random_engine_type        = Default_simulator_traits::random_engine_type;
-using Neuman_f_spin		= Neuman_flip_spin<numerical_type, random_engine_type>;
+using simulator_traits          = Default_simulator_traits;
+using Neuman_f_spin		= Neuman_flip_spin<simulator_traits>;
+using numerical_type		= Neuman_f_spin::numerical_type;
+using random_engine_type	= Neuman_f_spin::random_engine_type;
 using base_pointer		= Neuman_f_spin::base_pointer;
 using const_base_pointer	= Neuman_f_spin::const_base_pointer;
+using spin_params_type          = spin_params<Neuman_f_spin, simulator_traits>;
 
 numerical_type tolerance = 1e-5;//tolerance for BOOST_CHECK_CLOSE_FRACTION.
 
 random_engine_type random_generator(53);
 numerical_type tempreture(1), magnetic_flux_density(1), spin_interaction(1);
-const Neuman_f_spin up_spin(1, tempreture, magnetic_flux_density,
-			       spin_interaction, random_generator), 
-    down_spin(-1, tempreture, magnetic_flux_density, spin_interaction,
-	      random_generator);
+spin_params_type params(tempreture, magnetic_flux_density, spin_interaction);
+
+const Neuman_f_spin up_spin(1, params, random_generator), 
+    down_spin(-1, params, random_generator);
 const_base_pointer up_spin_p = &up_spin, down_spin_p = &down_spin;
 
 
@@ -31,14 +34,12 @@ BOOST_AUTO_TEST_CASE(Neuman_flip_spin_constructor)
     
     BOOST_CHECK_THROW(
 	Neuman_f_spin error_spin(
-	    2, tempreture,magnetic_flux_density, spin_interaction, random_generator),
-		      std::invalid_argument );
+	    2, params, random_generator), std::invalid_argument );
 }
 
 BOOST_AUTO_TEST_CASE(Neuman_flip_spin_current_energy)
 {
-    Neuman_f_spin test_spin(1, tempreture, magnetic_flux_density,
-			       spin_interaction, random_generator);
+    Neuman_f_spin test_spin(1, params, random_generator);
     
     std::array<const_base_pointer, 4> neighbours =
 	{up_spin_p, up_spin_p, up_spin_p, down_spin_p};    
@@ -48,8 +49,7 @@ BOOST_AUTO_TEST_CASE(Neuman_flip_spin_current_energy)
 
 BOOST_AUTO_TEST_CASE(Neuman_flip_spin_after_flip_energy)
 {
-    Neuman_f_spin test_spin(1, tempreture, magnetic_flux_density,
-			       spin_interaction, random_generator);
+    Neuman_f_spin test_spin(1, params, random_generator);
     
     std::array<const_base_pointer, 4> neighbours =
 	{up_spin_p, up_spin_p, up_spin_p, down_spin_p};    
@@ -59,8 +59,7 @@ BOOST_AUTO_TEST_CASE(Neuman_flip_spin_after_flip_energy)
 
 BOOST_AUTO_TEST_CASE(Neuman_flip_spin_step)
 {
-    Neuman_f_spin test_spin(1, tempreture, magnetic_flux_density,
-			       spin_interaction, random_generator);
+    Neuman_f_spin test_spin(1, params, random_generator);
     
     std::array<const_base_pointer, 4> neighbours =
 	{up_spin_p, up_spin_p, down_spin_p, down_spin_p};    
@@ -85,8 +84,7 @@ BOOST_AUTO_TEST_CASE(Neuman_flip_spin_step)
 
 BOOST_AUTO_TEST_CASE(Neuman_flip_spin_reset_state)
 {
-    Neuman_f_spin test_spin(1, tempreture, magnetic_flux_density,
-			       spin_interaction, random_generator);
+    Neuman_f_spin test_spin(1, params, random_generator);
     
     std::size_t total_step = 10000;
     numerical_type mean_state = test_spin.get();
