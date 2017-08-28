4de07b9944e879f7c71c2c910085e0b91457807d
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-28 22:53:19 +0900

    edit to use spin params and simplify template.

--- Ising_model/Ising_system.hpp
+++ Ising_model/Ising_system.hpp
@@ -5,16 +5,20 @@
 #include "spin.hpp"
 #include "system_initializer_func.hpp"
 
-template<typename spin_T, std::size_t row_Num, std::size_t column_Num, typename simulator_T>
+template<template<typename> typename spin_T,
+	 std::size_t row_Num, std::size_t column_Num, typename simulator_T>
 class Ising_system
 {
     using random_engine_type	= simulator_T::random_engine_type;
     using numerical_type	= simulator_T::numerical_type;
+    using spin_type             = spin_T<simulator_T>;
     using array_matrix		=
-	Utilpack::array_matrix<spin_T*, row_Num, column_Num>;
+	Utilpack::array_matrix<spin_type*, row_Num, column_Num>;
     
   public:
-    Ising_system(random_engine_type& ran_e):random_engine(ran_e)
+    Ising_system(spin_params<spin_type> s_params,
+		 random_engine_type& ran_e):
+	spin_params(s_params), random_engine(ran_e)
     {
 	typename
 	uniform_real_distribution<std::size_t>::param_type
@@ -26,14 +30,14 @@ class Ising_system
 
     void initialize()
     {
-	system_initialize<spin_T, row_Num, column_Num, simulator_T>
-	    (system, tempreture, magnetic_flux_density, spin_interaction);
+	system_initialize<spin_type, row_Num, column_Num>
+	    (system, spin_params);
 	return;
     }
     
     void reset_states()
     {
-	for(spin_T spin : system)
+	for(spin_type spin : system)
 	    spin.reset_state();
 	return;
     }
@@ -46,7 +50,7 @@ class Ising_system
     
   private:
     array_matrix system;
-    numerical_type tempreture, magnetic_flux_density, spin_interaction;
+    spin_params<spin_type> spin_params;
     random_engine_type& random_engine;
     std::uniform_int_distribution<std::size_t> dist_size;
 };
