d7f5a8b0918723bb338ab75238d0a4c73d53316f
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-29 19:25:29 +0900

    change first character of struct name to capital letter.

--- Ising_model/spin_params.hpp
+++ Ising_model/spin_params.hpp
@@ -2,18 +2,17 @@
 #define CELL_AUTOMATA_ISING_SPIN_PARAMS
 
 template<typename spin_T, typename simulator_T>
-struct spin_params;
-
+struct Spin_params;
 
 template<typename simulator_T>
 class Neuman_flip_spin;
 
 template<typename simulator_T>
-struct spin_params<Neuman_flip_spin<simulator_T>, simulator_T>
+struct Spin_params<Neuman_flip_spin<simulator_T>, simulator_T>
 {
     using numerical_type = typename simulator_T::numerical_type;
 
-    spin_params(numerical_type t, numerical_type m, numerical_type s):
+    Spin_params(numerical_type t, numerical_type m, numerical_type s):
 	tempreture(t), magnetic_flux_density(m), spin_interaction(s){};
     
     numerical_type tempreture, magnetic_flux_density, spin_interaction;
