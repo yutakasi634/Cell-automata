fda5b67426148d949be7ccc316f8b21b16729f23
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-29 19:23:59 +0900

    edit to rand generator to pointer and edit constructor. add get_partner

--- Ising_model/spin.hpp
+++ Ising_model/spin.hpp
@@ -12,31 +12,50 @@ template<typename simulator_T>
 class Neuman_flip_spin : public Spin_base<int, 4>
 {    
   public:
-    
-    using numerical_type	= typename simulator_T::numerical_type;
-    using random_engine_type	= typename simulator_T::random_engine_type;
+
+    using simulator_traits		= simulator_T;
+    using numerical_type		= typename simulator_traits::numerical_type;
+    using random_engine_type		= typename simulator_traits::random_engine_type;
+    using random_engine_pointer_type	= random_engine_type*;
+
+    Neuman_flip_spin(){};
     
     Neuman_flip_spin(
-	spin_params<Neuman_flip_spin<simulator_T>, simulator_T>& params,
-	random_engine_type& rand_e)
+	const Spin_params<Neuman_flip_spin<simulator_T>, simulator_T>& params,
+	const random_engine_pointer_type rand_engine_ptr):
+	Spin_base<int, 4>(1),
+	tempreture(params.tempreture),
+	magnetic_flux_density(params.magnetic_flux_density),
+	spin_interaction(params.spin_interaction),
+	random_engine_pointer(rand_engine_ptr)
     {
-	Neuman_flip_spin(1, params, rand_e);
+	generate_distribution();
 	reset_state();
     }
 	
     Neuman_flip_spin(
-	int s,
-	spin_params<Neuman_flip_spin<simulator_T>, simulator_T>& params,
-	random_engine_type& ran):
+	const int s,
+	const Spin_params<Neuman_flip_spin<simulator_T>, simulator_T>& params,
+	const random_engine_pointer_type random_engine_ptr):
 	Spin_base<int, 4>(s),
 	tempreture(params.tempreture),
 	magnetic_flux_density(params.magnetic_flux_density),
 	spin_interaction(params.spin_interaction),
-	random_engine(ran)
+	random_engine_pointer(random_engine_ptr)
+    {	
+	check_state();
+	generate_distribution();
+    }
+
+    void check_state() const
     {
-	if(s != 1 && s != -1)
+	if(state != 1 && state != -1)
 	    throw std::invalid_argument("Neuman flip spin state have to be 1 or -1.");
-	
+	return;
+    }
+
+    void generate_distribution()
+    {
 	typename
 	    std::uniform_real_distribution<numerical_type>::param_type param_r(0.0, 1.0);
 	dist_r01.param(param_r);
@@ -44,12 +63,18 @@ class Neuman_flip_spin : public Spin_base<int, 4>
 	typename
 	    std::uniform_int_distribution<>::param_type param_i(0, 1);
 	dist_i01.param(param_i);
+	return;
     }
-
+    
     virtual void set_partners(const partners_list_type& partners_list) override
     {
 	partners = partners_list;
 	return;
+    }
+
+    const partners_list_type& get_partners() const
+    {
+	return partners;
     }
     
     virtual void step() override
@@ -60,7 +85,7 @@ class Neuman_flip_spin : public Spin_base<int, 4>
 	else
 	{
 	    numerical_type flip_prob = std::exp(-tempreture * energy_diff);
-	    if(dist_r01(random_engine) <= flip_prob)
+	    if(dist_r01(*random_engine_pointer) <= flip_prob)
 		flip();
 	}
 	return;
@@ -88,12 +113,14 @@ class Neuman_flip_spin : public Spin_base<int, 4>
 
     virtual void reset_state() override
     {
-	state = 2 * dist_i01(random_engine) - 1;
+	state = 2 * dist_i01(*random_engine_pointer) - 1;
     }
+
+    
     
   private:
-    const numerical_type& tempreture, magnetic_flux_density, spin_interaction;
-    random_engine_type& random_engine;
+    numerical_type tempreture, magnetic_flux_density, spin_interaction;
+    random_engine_pointer_type random_engine_pointer;
     std::uniform_real_distribution<numerical_type> dist_r01;
     std::uniform_int_distribution<> dist_i01;
 };
