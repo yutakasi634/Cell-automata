7fec3cb7b1a4ac4676ad0511ef4124a01493c255
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-29 20:27:11 +0900

    add test for Ising system prototype.

---
+++ Ising_model/test_for_Ising_system.cpp
@@ -0,0 +1,24 @@
+#define BOOST_TEST_MAIN
+
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include "../utilpack/array_matrix.hpp"
+#include "Ising_simulator_traits.hpp"
+#include "Ising_system.hpp"
+
+using simulator_traits		= Default_simulator_traits;
+template<typename simulator_T>
+using spin_template		= Neuman_flip_spin<simulator_T>;
+using Ising_system_type		= Ising_system<spin_template, 3, 3, simulator_traits>;
+using numerical_type		= typename Default_simulator_traits::numerical_type;
+using random_engine_type	= typename Default_simulator_traits::random_engine_type;
+using spin_params_type		= Spin_params<spin_template<simulator_traits> >;
+
+numerical_type tempreture(1), magnetic_flux_density(1), spin_interaction(1);
+spin_params_type spin_params(tempreture, magnetic_flux_density, spin_interaction);
+random_engine_type random_engine(53);
+
+BOOST_AUTO_TEST_CASE(constructor_check_for_neuman_case)
+{
+    Ising_system_type test_system(spin_params, random_engine);
+}
