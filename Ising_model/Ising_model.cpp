fb5698af6eb1418feb23ba0cb7e61c009a178aac
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-30 23:24:58 +0900

    construct Simulator in heap memory.

--- Ising_model/Ising_model.cpp
+++ Ising_model/Ising_model.cpp
@@ -4,8 +4,8 @@
 #include "Ising_simulator_traits.hpp"
 
 using numerical_type = Default_simulator_traits::numerical_type;
-constexpr std::size_t system_row_num(1000);
-constexpr std::size_t system_column_num(1000);
+constexpr std::size_t system_row_num(10000);
+constexpr std::size_t system_column_num(10000);
 constexpr std::size_t total_step(10000);
 constexpr int random_seed(53);
 constexpr numerical_type tempreture(1);
@@ -13,14 +13,13 @@ constexpr numerical_type magnetic_flux_density(1);
 constexpr numerical_type spin_interaction(1);
 
 int main(){
+    auto Ising_simulator_ptr = new Ising_simulator<
+    	Neuman_flip_spin, system_row_num, system_column_num, Default_simulator_traits>
+    	(random_seed, tempreture, magnetic_flux_density, spin_interaction);
 
-    Ising_simulator<
-	Neuman_flip_spin, system_row_num, system_column_num, Default_simulator_traits>
-	simulator(random_seed, tempreture, magnetic_flux_density, spin_interaction);
-
-    // for(std::size_t step = 0; step < total_step; ++step){
-    // 	std::cout << step << std::endl;
-    // 	simulator.step();
-    // }
+    for(std::size_t step = 0; step < total_step; ++step){
+    	std::cout << step << std::endl;
+    	(*Ising_simulator_ptr).step();
+    }
     return 0;
 }
