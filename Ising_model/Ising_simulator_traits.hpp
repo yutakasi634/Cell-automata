a008981d6bb85b2f673d598997f0734d1eaac282
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-08-30 10:10:13 +0900

    add include <random>

--- Ising_model/Ising_simulator_traits.hpp
+++ Ising_model/Ising_simulator_traits.hpp
@@ -1,6 +1,8 @@
 #ifndef CELL_AUTOMATA_ISING_SIMULATOR_TRAITS
 #define CELL_AUTOMATA_ISING_SIMULATOR_TRAITS
 
+#include <random>
+
 struct Default_simulator_traits
 {
     using numerical_type	= float;
