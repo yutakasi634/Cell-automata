7f7a06004d17cc0d56b641be630b2a122501535b
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-30 21:25:06 +0900

    edit constexpression variable.

--- Ising_model/Ising_model.cpp
+++ Ising_model/Ising_model.cpp
@@ -4,31 +4,23 @@
 #include "Ising_simulator_traits.hpp"
 
 using numerical_type = Default_simulator_traits::numerical_type;
-constexpr std::size_t system_row_num, system_column_num, total_step;
-constexpr int random_seed;
-constexpr numerical_type tempreture, magnetic_flux_density, spin_interaction;
-
-row_Num			= 1000;
-column_Num		= 1000;
-total_step		= 10000;
-random_seed		= 53;
-tempreture		= 1;
-magnetic_flux_density	= 1;
-spin_interaction	= 1;
+constexpr std::size_t system_row_num(1000);
+constexpr std::size_t system_column_num(1000);
+constexpr std::size_t total_step(10000);
+constexpr int random_seed(53);
+constexpr numerical_type tempreture(1);
+constexpr numerical_type magnetic_flux_density(1);
+constexpr numerical_type spin_interaction(1);
 
 int main(){
 
     Ising_simulator<
 	Neuman_flip_spin, system_row_num, system_column_num, Default_simulator_traits>
-	simulator(random_seed);
-    simulator.set_neuman_spin_system(
-	tempreture, magnetic_flux_density, spin_interaction);
+	simulator(random_seed, tempreture, magnetic_flux_density, spin_interaction);
 
-    for(std::size_t step = 0; step < total_step; ++step){
-	simulator.step();
-	    }
-    
-    
+    // for(std::size_t step = 0; step < total_step; ++step){
+    // 	std::cout << step << std::endl;
+    // 	simulator.step();
+    // }
     return 0;
-
 }
