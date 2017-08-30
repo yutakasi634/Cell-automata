0e0aa6ed6c5898bc5e5a7f98d6f2d1d76b23dbaa
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-30 21:22:14 +0900

    ajust to pointer ver spin and correct default constructor.

--- Ising_model/Ising_system.hpp
+++ Ising_model/Ising_system.hpp
@@ -11,29 +11,39 @@ class Ising_system
 {
     using spin_type			= spin_T;
     using spin_params_type              = Spin_params<spin_type>;
+    using spin_params_pointer           = spin_params_type*;
     using simulator_traits              = typename spin_type::simulator_traits;
     using random_engine_type		= typename simulator_traits::random_engine_type;
+    using random_engine_pointer         = random_engine_type*;
     using numerical_type		= typename simulator_traits::numerical_type;
     using array_matrix			=
 	Utilpack::array_matrix<spin_type, row_Num, column_Num>;
     
   public:
-    Ising_system(spin_params_type& s_params,
-		 random_engine_type& ran_e):
-	spin_params(s_params), random_engine(ran_e)
+    Ising_system():spin_params_ptr(nullptr), random_engine_ptr(nullptr)
     {
 	typename
 	    std::uniform_int_distribution<std::size_t>::param_type
 	    param(0, system.size() - 1);
 	dist_size.param(param);
-	
-	initialize();
-    }
-
-    void initialize()
+    };
+    
+    Ising_system(spin_params_type& s_params,
+		 random_engine_type& ran_e)
     {
+	typename
+	    std::uniform_int_distribution<std::size_t>::param_type
+	    param(0, system.size() - 1);
+	dist_size.param(param);
+	initialize(s_params, ran_e);
+    }
+    
+    void initialize(spin_params_type& s_params, random_engine_type& ran_e)
+    {
+	spin_params_ptr = &s_params;
+	random_engine_ptr = &ran_e;
 	system_initialize<spin_type, row_Num, column_Num>
-	    (system, spin_params, &random_engine);
+	    (system, spin_params_ptr, random_engine_ptr);
 	return;
     }
     
@@ -46,19 +56,19 @@ class Ising_system
     
     void step()
     {
-	system.at(dist_size(random_engine)).step();
+	system.at(dist_size(*random_engine_ptr)).step();
 	return;
     }
 
-    array_matrix& spins_state() const
+    const array_matrix& spins_state() const
     {
 	return system;
     }
     
   private:
     array_matrix system;
-    Spin_params<spin_type>& spin_params;
-    random_engine_type& random_engine;
+    spin_params_pointer spin_params_ptr;
+    random_engine_pointer random_engine_ptr;
     std::uniform_int_distribution<std::size_t> dist_size;
 };
 
