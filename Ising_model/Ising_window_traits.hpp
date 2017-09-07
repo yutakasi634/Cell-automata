b7b2e6a768a45d42bac43c9e5c68d392a927800c
yutaka <p7dea87c7e4n7j@gmail.com>
2017-09-07 22:42:39 +0900

    add Ising_window_traits.hpp

---
+++ Ising_model/Ising_window_traits.hpp
@@ -0,0 +1,16 @@
+#ifndef CELL_AUTOMATA_OPENGLWRAPPER_DRAWISING_TAITS
+#define CELL_AUTOMATA_OPENGLWRAPPER_DRAWISING_TAITS
+
+#include <string>
+
+struct Ising_window_traits
+{
+    constexpr static const char* window_name = "ising_model";
+    constexpr static int window_x_size = 1000;
+    constexpr static int window_y_size = 1000;
+    constexpr static double margin = 0.1;
+};
+    
+    
+
+#endif /* CELL_AUTOMATA_OPENGLWRAPPER_DRAWISING_TAITS */
