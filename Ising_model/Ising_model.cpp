53f357c14caab6136329ba6e72f80537f502560a
yutaka <p7dea87c7e4n7j@gmail.com>
2017-09-09 13:31:24 +0900

    replace function and global variable to other file

--- Ising_model/Ising_model.cpp
+++ Ising_model/Ising_model.cpp
@@ -4,48 +4,10 @@
 #include "spin.hpp"
 #include "Ising_simulator_traits.hpp"
 #include "Ising_window_traits.hpp"
-#include "../openGLwrapper/make_canvas.hpp"
-#include "../openGLwrapper/draw_figure.hpp"
+#include "Ising_draw_func.hpp"
+#include "Ising_model_parameters.hpp"
+#include "Ising_global_val.hpp"
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~parameter set~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-using numerical_type = Default_simulator_traits::numerical_type;
-
-constexpr std::size_t system_side_num(1000);
-
-constexpr std::size_t interval_calculation(system_side_num * 100);
-
-constexpr int random_seed(53);
-constexpr numerical_type tempreture(1);
-constexpr numerical_type magnetic_flux_density(0.0);
-constexpr numerical_type spin_interaction(1.0);
-
-using simulator_type = Ising_simulator<
-    Neuman_flip_spin, system_side_num, system_side_num, Default_simulator_traits>;
-
-//~~~~~~~~~~~~~~~~~~~~~~~~global func declaration~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-template<typename window_traits>
-void set_canvas(int& argc, char** argv);
-
-void update_canvas(bool);
-
-void draw_canvas();
-
-template<typename window_traits>
-void draw_pixel();
-
-template<typename window_traits>
-void display();
-
-void output_file(std::size_t);
-//~~~~~~~~~~~~~~~~~~~~~~~~global variable declaration~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-const simulator_type::array_matrix* Ising_field_ptr(nullptr);
-simulator_type* Ising_simulator_ptr(nullptr); 
-std::size_t step(0);
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~main~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int main(int argc, char *argv[]){
     Ising_simulator_ptr = new simulator_type
     	(random_seed, tempreture, magnetic_flux_density, spin_interaction);
@@ -54,79 +16,11 @@ int main(int argc, char *argv[]){
     
     set_canvas<Ising_window_traits>(argc ,argv);    
     update_canvas(true);
-    glutMainLoop();
-    
+    update_roop();
+
     delete Ising_simulator_ptr;
+    
     return 0;
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~global func definition~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-template<typename window_traits>
-void set_canvas(int& argc, char** argv)
-{
-    setWindow<window_traits>(argc, argv);
-    setBackground();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutDisplayFunc(display<window_traits>);
-    return;
-}
-
-template<typename window_traits>
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    draw_pixel<window_traits>();
-    glutSwapBuffers();
-    return;
-}
-
-void update_canvas(bool flag)
-{
-    if(flag)
-	glutIdleFunc(draw_canvas);
-    else
-	glutIdleFunc(0);
-    return;
-}
-    
-void draw_canvas()
-{
-    ++step;
-    std::cout << step * interval_calculation << " ";
-    for(std::size_t roop = 0; roop < interval_calculation; ++roop)
-	Ising_simulator_ptr->step();
-    glutPostRedisplay();
-    std::cout << std::endl;
-    return;
-}
-
-template<typename window_traits>
-void draw_pixel()
-{
-    const std::size_t row_n = Ising_field_ptr->row_num();
-    const std::size_t column_n = Ising_field_ptr->column_num();
-    constexpr float margin = window_traits::margin;
-    constexpr float frame_size = 2 - 2.0 * margin;
-    
-    const double x_point_gap = frame_size / row_n;
-    const double y_point_gap = frame_size / column_n;
-    const float point_size = window_traits::window_side / system_side_num;
-
-    const double initial_x_coord = -1 + margin + x_point_gap * 0.5;
-    const double initial_y_coord = -1 + margin + y_point_gap * 0.5;
-    
-    double x_coord = initial_x_coord;
-    double y_coord = initial_y_coord;
-    for(std::size_t row = 0; row < row_n; ++row){
-	for(std::size_t column = 0; column < column_n; ++column){
-	    if(Ising_field_ptr->at(row, column).get() == 1)
-		drawpoint(x_coord, y_coord, point_size);
-	    x_coord += x_point_gap;
-	}
-	x_coord = initial_x_coord;
-	y_coord += y_point_gap;
-    }
-    return;
-}
 
