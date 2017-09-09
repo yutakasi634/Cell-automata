d44d5dec1f217e9b7cb0c3983cbc416925d0f1be
yutaka <p7dea87c7e4n7j@gmail.com>
2017-09-09 13:35:32 +0900

    seprate global variable declaration

---
+++ Ising_model/Ising_global_variable.hpp
@@ -0,0 +1,10 @@
+#ifndef CELL_AUTOMATA_ISING_SYSTEM_GLOVAL_VALIABLE
+#define CELL_AUTOMATA_ISING_SYSTEM_GLOVAL_VALIABLE
+
+#include "Ising_model_parameters.hpp"
+
+const		 simulator_type::array_matrix* Ising_field_ptr(nullptr);
+simulator_type*	 Ising_simulator_ptr(nullptr); 
+std::size_t	 step(0);
+
+#endif /* CELL_AUTOMATA_ISING_SYSTEM_GLOVAL_VALIABLE */
